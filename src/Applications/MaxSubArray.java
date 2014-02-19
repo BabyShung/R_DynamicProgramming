0761e2b738451317d05f2bdd538ed4842a9203a7
BabyShung <hao-zheng@uiowa.edu>
2014-02-18 19:47:58 -0600

    added intro

--- src/Applications/MaxSubArray.java
+++ src/Applications/MaxSubArray.java
@@ -1,6 +1,9 @@
 package Applications;
 
 /**
+ * Given an int array (elements can be both positive and negative,
+ * return the maximum consecutive largest sum
+ * 
  * 
  * M(k) = max(M(k-1)+Ak,Ak)
  * 
@@ -22,7 +25,7 @@ public class MaxSubArray {
 
 		for (int i = 1; i < A.length; i++) {
 			buffer[i] = Math.max(A[i], buffer[i - 1] + A[i]);
-			max = Math.max(buffer[i], max);
+			max = Math.max(buffer[i], max);	//keep track of the largest sum
 		}
 		return max;
 	}
